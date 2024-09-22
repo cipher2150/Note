62e1ff3f0a060b58381fa2f41bf5198744597d09
cipher2150 <cipher2150@gmail.com>
2024-09-22 10:53:36 +0530

    Minimal prototype is ready

--- jsfile.js
+++ jsfile.js
@@ -1,13 +1,23 @@
 let notesContainer = document.getElementById("notesContainer");
-//let notesContainer = document.getElementById("test");
+let displayToggle = document.getElementById("test");
 let textAreaEl = document.getElementById("textArea");
+let firstPhase = document.getElementById("firstPhase");
+
+
 
 let saveBtn = document.getElementById("saveBtn");
 
+let count = 1;
+
 function save(){
-    let textAreaValue = textArea.value;
+    let textAreaValue = textAreaEl.value;
+
+    let uniqueId = "para" + count;
+    count++;
+    
 
     let notesContainerEl = document.createElement('div');
+    notesContainerEl.setAttribute("id", uniqueId);
     notesContainerEl.classList.add("notes-container", "d-flex", "flex-row");
     notesContainer.appendChild(notesContainerEl);
 
@@ -25,10 +35,16 @@ function save(){
     notesContainerEl.appendChild(iconContainer);
 
     let iconEl = document.createElement('i');
-    iconEl.classList.add('fa-solid', 'fa-trash');
+    iconEl.classList.add("fa-solid", "fa-trash");
+    iconEl.setAttribute("id", uniqueId);
+    iconEl.onclick = () => {
+        notesContainer.removeChild(notesContainerEl);
+    }
     iconContainer.appendChild(iconEl);
 
     textAreaEl.value = "";
+    
+
 }
 
 saveBtn.onclick = save;
